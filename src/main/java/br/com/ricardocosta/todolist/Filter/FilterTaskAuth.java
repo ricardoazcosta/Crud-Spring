508a82a04ac4ad56525ca214a409b2504d084aa1
Ricardo Alexandre Costa <ricardozanete@gmail.com>
2023-10-12 01:48:29 -0300

    :construction: Criação de Password Code

--- src/main/java/br/com/ricardocosta/todolist/Filter/FilterTaskAuth.java
+++ src/main/java/br/com/ricardocosta/todolist/Filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.ricardocosta.todolist.Filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.ricardocosta.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -30,20 +36,28 @@ public class FilterTaskAuth extends OncePerRequestFilter{
                  String[] credentials = authString.split(":");
                  String username = credentials[0];
                  String password = credentials[1];
-                 System.out.println("Authorization");
-                 System.out.println(username);
-                 System.out.println(password);
+                 
 
 
 
 
                 // Validar usuario
+var user = this.userRepository.findByUsername(username);
+if(user == null){
+    response.sendError(401);
+}else{
 
-                //Validar senha
-
-                // Tudo Ok 
-
+    //Validar senha
+   var passwordVerify = BCrypt.verifyer().verify(password.toCharArray() , user.getPassword());
+  if(passwordVerify.verified){
        filterChain.doFilter(request, response);
+
+   }else{
+    response.sendError(401);
+   }
+    // Tudo Ok 
+
+}
     }
 
     
