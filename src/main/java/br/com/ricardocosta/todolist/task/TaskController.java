b59e986015898a1e5ad7b90cfdf03a1974e8936d
Ricardo Alexandre Costa <ricardozanete@gmail.com>
2023-10-17 00:20:40 -0300

    :tada: Add metodo validação de usuario dono

--- src/main/java/br/com/ricardocosta/todolist/task/TaskController.java
+++ src/main/java/br/com/ricardocosta/todolist/task/TaskController.java
@@ -68,14 +68,29 @@ public class TaskController {
 
     //http://localhost:8080/task/...
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
         
       var task = this.taskRepository.findById(id).orElse(null);
 
+      if (task == null) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body("Tarefa não existe");
+        
+      }
+
+      var idUser = request.getAttribute("idUser");
+
+      if(!task.getIdUser().equals(idUser)){
+
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+           .body("Usuario não tem permissão para alterar esse tarefa");
+
+        }
+
       Utils.copyNonNullProperties(taskModel, task);
         
-       
-       return this.taskRepository.save(task);
+       var taskUpDate = this.taskRepository.save(task);
+       return ResponseEntity.ok().body(taskUpDate);
 
 
     }
